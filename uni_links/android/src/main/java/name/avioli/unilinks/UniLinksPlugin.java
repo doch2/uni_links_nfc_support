a7c9ee105a149228ea762e4c682b7e4e4182c92a
doch13_ <0922happy65@gmail.com>
2022-04-30 22:54:06 +0900

    Add: Android NFC Support

--- uni_links/android/src/main/java/name/avioli/unilinks/UniLinksPlugin.java
+++ uni_links/android/src/main/java/name/avioli/unilinks/UniLinksPlugin.java
@@ -3,6 +3,7 @@ package name.avioli.unilinks;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.nfc.NfcAdapter;
 
 import androidx.annotation.NonNull;
 
@@ -36,7 +37,7 @@ public class UniLinksPlugin
         String action = intent.getAction();
         String dataString = intent.getDataString();
 
-        if (Intent.ACTION_VIEW.equals(action)) {
+        if (Intent.ACTION_VIEW.equals(action) || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
             if (initialIntent) {
                 initialLink = dataString;
                 initialIntent = false;
